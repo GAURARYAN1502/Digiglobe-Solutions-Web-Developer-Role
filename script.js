f22a55920d61f8388ffb2b98796bf17949b8a55a
Aryan Gaur <145830371+GAURARYAN1502@users.noreply.github.com>
2024-10-14 11:58:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer . . ." , "Python Programmer . . ." , "Content Creator. . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
